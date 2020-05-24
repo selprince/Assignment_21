35dbc6c1a1eb966d3ad75ec27ce531b0b75f08bd
selprince <55705084+selprince@users.noreply.github.com>
2020-05-24 07:12:50 -0400

    Update PupperCoinTokenSale.sol

--- PupperCoinTokenSale.sol
+++ PupperCoinTokenSale.sol
@@ -15,21 +15,16 @@ contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCro
         uint rate, // rate, in TKNbits
         address payable wallet, // wallet to send ether
         PupperCoin token, //the token
-        uint cap, //total cap in wei
+        uint goal, //total cap in wei
         uint openingTime, // opening time in unix epoch seconds
         uint closingTime // closing time in unix epoch seconds
-        //uint goal // the minimum token raise in wei
-        //string memory name, // name for your token
-        //string memory symbol, // symbol for your token
-        
-        
+       
     )
         Crowdsale(rate,wallet,token) 
-        CappedCrowdsale(cap)
+        CappedCrowdsale(goal)
         TimedCrowdsale(openingTime,closingTime)
-        RefundableCrowdsale(cap)
-        //PostDeliveryCrowdsale() //distributes tokens after the crowdsale has finished
-        //MintedCrowdsale(name, tokenAmount)
+        RefundableCrowdsale(goal)
+        
         public
     {
         // constructor can stay empty
@@ -48,13 +43,8 @@ contract PupperCoinSaleDeployer {
         string memory name,
         string memory symbol,
         address payable wallet,
-        uint cap
-        //uint goal
-        //uint256 rate,
-        //IERC20 token,
-        //uint openingTime,
-        //uint closingTime,
-    
+        uint goal
+        
     )
         public
     {
@@ -62,10 +52,16 @@ contract PupperCoinSaleDeployer {
             PupperCoin token = new PupperCoin(name,symbol,0);
             token_address = address(token);
         // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
-            PupperCoinSale pupper_sale = new PupperCoinSale(1, wallet, token, cap, now, now + 24 weeks);
+            PupperCoinSale pupper_sale = new PupperCoinSale(1, wallet, token, goal, now, now + 3 minutes);
             token_sale_address = address(pupper_sale);
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
         token.renounceMinter();
     }
     
+       
+    }
+    
+    
+   
+ 
